dfba150f8d6398b5859ef49255c06ee89c7cf9aa
Daii(arian) <arian.gmohamdi@gmail.com>
2023-02-25 21:58:24 +0330

    Initial Commit

---
+++ SaniTech/SaniTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaniTechApp.swift
+//  SaniTech
+//
+//  Created by arian on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SaniTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
